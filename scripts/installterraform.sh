0e803b308f9935caf4df0d3751c7b799d81e71c2
Maksym Zaloznyi <maksim.zaloznuy@gmail.com>
2020-03-10 13:06:08 +0200

    Change script/.sh

--- scripts/installterraform.sh
+++ scripts/installterraform.sh
@@ -2,7 +2,7 @@
 set -e
 set -x
 curl https://sdk.cloud.google.com > install.sh
-bash install.sh --disable-prompts
+source  install.sh --disable-prompts
 exec -l $SHELL
 apt-get install unzip
 wget https://releases.hashicorp.com/terraform/0.12.20/terraform_0.12.20_linux_amd64.zip
@@ -15,8 +15,6 @@ git clone https://github.com/DanJake/terraform_sentry.git /home/rasavo99/terrafo
 git clone https://github.com/ashmrkvch/alerts.git /home/rasavo99/terraform/alerts
 gsutil -m cp -r gs://terraform_state_demo3/terraform.tfvars /home/rasavo99/terraform/sentry
 mkdir -p /home/rasavo99/terraform/sentry/.terraform/plugins/linux_amd64
-chown -R rasavo99:rasavo99 /home/rasavo99/terraform/all
-chown -R rasavo99:rasavo99 /home/rasavo99/terraform/sentry
 cd /home/rasavo99/terraform/sentry/.terraform/plugins/linux_amd64
 wget https://github.com/jianyuan/terraform-provider-sentry/releases/download/v0.5.3/terraform-provider-sentry_0.5.3_linux_amd64.tar.gz
 tar -xvzf terraform-provider-sentry_0.5.3_linux_amd64.tar.gz
